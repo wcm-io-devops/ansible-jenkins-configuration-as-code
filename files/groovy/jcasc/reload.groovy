035f9ae2f68fe8ef712bb80bb02d650ae48f2260
Tobias Richter <trichter@pro-vision.de>
2018-11-13 09:39:11 +0100

    Fail on config reload when config path is not set in jcasc plugin

--- files/groovy/jcasc/reload.groovy
+++ files/groovy/jcasc/reload.groovy
@@ -20,18 +20,29 @@
 
 
 import groovy.json.JsonOutput
-import io.jenkins.plugins.casc.ConfigurationAsCode
+import io.jenkins.plugins.casc.*
+
+CasCGlobalConfig config = GlobalConfiguration.all().get(CasCGlobalConfig.class)
+String currentConfigPath = config.getConfigurationPath()
 
 String msg = "Success"
 Boolean failed = false
 
-try {
-  ConfigurationAsCode.get().configure()
-} catch (Exception ex) {
-  // set failed to yes
+if (currentConfigPath != null) {
+  try {
+    ConfigurationAsCode.get().configure()
+  } catch (Exception ex) {
+    // set failed to yes
+    failed = true
+    msg = ex.toString()
+  }
+} else {
+  // fail because no config path is set
   failed = true
-  msg = ex.toString()
+  msg = "JCasC as currently no configuration path set! Reloading configuration skipped. Make sure to set a correct configuration path!"
 }
+
+
 
 def json = JsonOutput.toJson([
   failed: failed,
